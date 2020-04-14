254b9d5dc2fcc8fc87cc8cdeb5bd965b14868828
Panchajanya <panchajanyamysarla@gmail.com>
2020-04-14 14:33:40 -0500

    intial

---
+++ BlobTrigger1/index.js
@@ -0,0 +1,32 @@
+
+var Jimp = require("jimp");
+
+module.exports = (context, myBlob) => {
+
+    // Read image with Jimp
+    Jimp.read(myBlob).then((image) => {
+
+        // Manipulate image
+        image
+            .resize(200, Jimp.AUTO) 
+            .greyscale()
+            .getBuffer(Jimp.MIME_JPEG, (error, stream) => {
+
+                // Check for errors
+                if (error) {
+                    context.log(`There was an error processing the image.`);
+                    context.done(error);
+                }
+                else {
+                    context.log(`Successfully processed the image`);
+
+                    // Bind the stream to the output binding to create a new blob
+                    context.done(null, stream);
+
+                }
+
+            });
+
+    });
+
+};
